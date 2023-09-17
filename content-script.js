f789580ba495c2c1d22b13c36113acc6ff4c7ef7
AndriyKutsak <andriykutsak.ak@gmail.com>
2023-09-17 19:21:05 +0300

    add testing version

--- content-script.js
+++ content-script.js
@@ -6,214 +6,260 @@ head.appendChild(meta);
 let loginInp = document.querySelector("#loginB1");
 
 if (loginInp === null) {
-    console.log("auth")
-    let parser = new DOMParser();
-    let select = document.querySelector(".search .select2-selection");
-    let searchInp = document.querySelector("#search");
-    let searchInpBtn = document.querySelector(".search input[type='button']");
-    let div = Array.from(document.querySelectorAll("div")) || [0];
-    let btnsWraper = document.querySelector(".buttons");
-    let selectButtons = Array.from(document.querySelectorAll('.buttons input[type="button"]')) || [0];
-    let buttonsToggler = document.createElement("button");
-    let tableWraper = document.querySelector("#mainDiv");
-    let elaborationURL = "https://baza.m-p.in.ua/ajax/loadElaboration.php";
-    let orderURL = ""
-    let newTableWraper = document.createElement("div");
-    newTableWraper.className = "new-table-wraper";
-    let elaborationLink = null;
-    buttonsToggler.textContent = "Опції";
-    buttonsToggler.className = "buttons-toggler";
-    btnsWraper.prepend(buttonsToggler);
-    document.body.appendChild(newTableWraper);
-    tableWraper.style.display = "none";
-    // set select width
-    select.style.width = "100%";
-    // set styles for search input
-    searchInp.style.padding = "10px";
-    searchInp.style.fontsize = "18px";
-    searchInp.style.fontWeight = "600";
-    // set styles search input button
-    searchInpBtn.style.width = "20%";
-    searchInpBtn.style.padding = "10px";
-    // remove padding and set width 100%
-    btnsWraper.parentNode.style.paddingLeft = "0px";
-    btnsWraper.parentNode.style.width = "100%";
-    // set top margin for buttons wraper
-    btnsWraper.style.marginTop = "70px";
-    btnsWraper.classList.add("hide-buttons");
+	let parser = new DOMParser();
+	let select = document.querySelector(".search .select2-selection");
+	let searchInp = document.querySelector("#search");
+	let searchInpBtn = document.querySelector(".search input[type='button']");
+	let div = Array.from(document.querySelectorAll("div")) || [0];
+	let btnsWraper = document.querySelector(".buttons");
+	let selectButtons = Array.from(
+		document.querySelectorAll('.buttons input[type="button"]')
+	) || [0];
+	let buttonsToggler = document.createElement("button");
+	let tableWraper = document.querySelector("#mainDiv");
+	let elaborationURL = "https://baza.m-p.in.ua/ajax/loadElaboration.php";
+	let searchURL = "https://baza.m-p.in.ua/ajax/search.php";
+	let orderURL = "";
 
-    // remove blue div
-    div.forEach((el) => {
-        if (el.textContent.includes("Склад (Василенка)") && el.style.borderRadius == "5px") {
-            el.style.display = "none";
-        }
-    })
-    buttonsToggler.addEventListener('click', () => {
-        btnsWraper.classList.toggle("hide-buttons");
-    });
+	let elaborationLink = null;
+	buttonsToggler.textContent = "Опції";
+	buttonsToggler.className = "buttons-toggler";
+	btnsWraper.prepend(buttonsToggler);
 
+	tableWraper.style.display = "none";
+	// set select width
+	select.style.width = "100%";
+	// set styles for search input
+	searchInp.style.padding = "10px";
+	searchInp.style.fontsize = "18px";
+	searchInp.style.fontWeight = "600";
+	// set styles search input button
+	searchInpBtn.style.width = "20%";
+	searchInpBtn.style.padding = "10px";
+	// remove padding and set width 100%
+	btnsWraper.parentNode.style.paddingLeft = "0px";
+	btnsWraper.parentNode.style.width = "100%";
+	// set top margin for buttons wraper
+	btnsWraper.style.marginTop = "70px";
+	btnsWraper.classList.add("hide-buttons");
 
-    let parseGetedHTML = (data) => {
-        let doc = parser.parseFromString(data, 'text/html');
-        return doc;
-    }
-    // elaboration
+	// remove blue div
+	div.forEach((el) => {
+		if (
+			el.textContent.includes("Склад (Василенка)") &&
+			el.style.borderRadius == "5px"
+		) {
+			el.style.display = "none";
+		}
+	});
+	buttonsToggler.addEventListener("click", () => {
+		btnsWraper.classList.toggle("hide-buttons");
+	});
 
+	let parseGetedHTML = (data) => {
+		let doc = parser.parseFromString(data, "text/html");
+		return doc;
+	};
+	// elaboration
+	let getLink = (link) => {
+		var regex = /\/\d+_\d+\//;
+		var replacedString = link.replace(regex, "/");
+		return replacedString;
+	};
+	let loadElaboration = () => {
+		console.log("Завантаження розширення розпочалося");
+		let elaborationData;
+		let newTableWraper = document.createElement("div");
+		newTableWraper.className = "new-table-wraper";
+		let data = {};
+		fetch(elaborationURL, {
+			method: "POST",
+		})
+			.then((response) => {
+				return response.text();
+			})
+			.then((response) => {
+				elaborationData = response;
+				elaborationData = parseGetedHTML(elaborationData);
 
-    let loadElaboration = () => {
-        console.log("load elaboration was started")
-        let elaborationData, data = {};
-        fetch(elaborationURL, {
-            method: "POST",
-        }).then((responce) => {
-            return responce.text();
-        }).then((responce) => {
-            elaborationData = responce;
-            elaborationData = parseGetedHTML(elaborationData);
-            let tr = Array.from(elaborationData.querySelectorAll("table>tbody>tr"));
-            tr.forEach((el, key) => {
-                data[key] = [];
-                if (key >= 0) {
-                    let td = Array.from(el.querySelectorAll("td"));
-                    for (let iterator of td) {
-                        data[key].push(iterator.textContent)
-                        console.log(data);
+				let cellNames = [
+					"orderNumber",
+					"orderManager",
+					"positionName",
+					"positionPlace",
+					"positionQuality",
+					"reserveQuality",
+					"searchQuery",
+					"imagesSrc",
+					"imageLink",
+				];
+				let tableRows = Array.from(
+					elaborationData.querySelectorAll("table>tbody>tr")
+				);
 
-                    }
-                }
-                generateTable(data)
-            });
-            return false;
-        }).catch((err) => {
-            alert("Не вдалось отримати уточняк!!");
-            console.log(err);
-        })
-        let generateTable = (data) => {
-            console.log(data)
-            data.forEach((text) => {
-                let table = document.createElement("div");
-                table.className = "table";
-                let tableHead = `<div class="table-head">
-        <div>
-            <p class="table-head-desc">Номер Замовлення</p>
-        </div>
-        <div>
-            <p class="table-head-desc">Менеджер</p>
-        </div>
-        <div>
-            <p class="table-head-desc">Товар</p>
-        </div>
-        <div>
-            <p class="table-head-desc">Адрес товару</p>
-        </div>
-        <div>
-            <p class="table-head-desc">Відповідь</p>
-        </div>
-        <div>
-            <p class="table-head-desc">Резерв</p>
-        </div>
-        <div>
-            <p class="table-head-desc">Фото товару</p>
-        </div>
-    </div>`
-                let tableBody = document.createElement("div");
-                tableBody.className = "table-head";
-                let tableOrderNumber = document.createElement("div");
-                tableOrderNumber.className = "order-number";
-                tableOrderNumber.textContent = data.orderNumber;
-                let tabbleOrderManager = document.createElement("div");
-                tabbleOrderManager.className = "order-manager";
-                tabbleOrderManager.textContent = data.orderManager
-                let tableOredrPositionName = document.createElement("div");
-                tableOredrPositionName.className = "order-position-name";
-                tableOredrPositionName.textContent = data.positionName;
-                let tablePositionPlace = document.createElement("div");
-                tablePositionPlace.className = "position-place";
-                tablePositionPlace.textContent = data.positionPlace;
-                let tableOrderPositionQuality = document.createElement("div");
-                tableOrderPositionQuality.className = "order-positon-quality";
-                tableOrderPositionQuality.textContent = data.positionQuality;
-                let tableReserveOrderQuality = document.createElement("div");
-                tableReserveOrderQuality.className = "order-reserve-quality";
-                tableReserveOrderQuality.textContent = data.reserveQuality;
-                let tablePartImnages = document.createElement("div");
-                tablePartImnages.className = "part-image-wraper";
-                // generating table
-                table.innerHTML = tableHead;
-                tableBody.append(tableOrderNumber);
-                tableBody.append(tabbleOrderManager);
-                tableBody.append(tableOredrPositionName);
-                tableBody.append(tablePositionPlace);
-                tableBody.append(tableOrderPositionQuality);
-                tableBody.append(tableReserveOrderQuality);
-                tableBody.append(tablePartImnages);
-                table.appendChild(tableBody);
-                newTableWraper.appendChild(table);
-            });
-        }
+				data = tableRows.map((row) => {
+					let rowData = {};
+					let rowCells = Array.from(row.querySelectorAll("td"));
 
+					rowCells.forEach((cell, index) => {
+						rowData[cellNames[index]] = cell.textContent.trim();
+						if (cellNames[index] == "searchQuery") {
+							let result = rowData["positionName"].replace(/\([^)]+\)/g, "");
+							rowData[cellNames[index]] = result.trim();
+						}
+					});
 
+					return rowData;
+				});
 
-    };
-    loadElaboration()
-    function search(search_sel, search, page) {
-        if (typeof search == 'undefined')
-            var search = $('#search').val();
-        if (typeof search_sel == 'undefined') {
-            var search_sel = $('#search_sel').val();
-            var test = '0';
-        }
-        else var test = 1;
+				data.forEach((query, key) => {
+					if (key == 0) {
+						return false;
+					}
+					var request = new URLSearchParams();
+					request.append("search", query.searchQuery);
+					request.append("search_sel", "0");
+					fetch(searchURL, {
+						method: "POST",
+						headers: {
+							Accept: "*/*",
+							"Accept-Encoding": "gzip, deflate, br",
+							"Accept-Language": "uk-UA,uk;q=0.9,en-US;q=0.8,en;q=0.7",
+							"Sec-Ch-Ua":
+								'"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+							"Sec-Ch-Ua-Mobile": "?1",
+							"Sec-Ch-Ua-Platform": '"Android"',
+							"Sec-Fetch-Dest": "empty",
+							"Sec-Fetch-Mode": "cors",
+							"Sec-Fetch-Site": "same-origin",
+							"User-Agent":
+								"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Mobile Safari/537.36",
+							"X-Requested-With": "XMLHttpRequest",
+							"Content-Type":
+								"application/x-www-form-urlencoded; charset=UTF-8",
+						},
+						body: request,
+					})
+						.then((responce) => {
+							return responce.text();
+						})
+						.then((responce) => {
+							let searchresult = parseGetedHTML(responce);
+							let reserve = searchresult.querySelectorAll(".detPr >span")[1];
+							let images = Array.from(
+								searchresult.querySelectorAll(".detImg>img")
+							);
+							let imgSrc = [];
+							let imgLink = [];
+							images.forEach((img) => {
+								imgSrc.push(img.rel);
+								imgLink.push(`https://baza.m-p.in.ua${img.alt}`);
+							});
+							data.reserveQuality = reserve.textContent;
+							data.imagesSrc = imgSrc;
+							generateTable(data);
+						});
+				});
+			})
+			.catch((err) => {
+				alert("Не вдалося отримати уточнення!!");
+				console.log(err);
+			});
+		let getOrderId = (data) => {
+			// Визначаємо регулярний вираз для пошуку числа в дужках
+			var regex = /\((\d+)\)/;
 
-        if (page == 'undefined') var page = 0;
+			// Знаходимо відповідність регулярному виразу в рядку
+			var match = data.match(regex);
 
-        if (search.length > 1 || test == 1) {
-            $('#loader').show();
-            $.post("https://baza.m-p.in.ua/ajax/search.php", { search: search, search_sel: search_sel, page: page }, function (data) {
-                $('#mainDiv').html(data);
-                $('#loader').hide();
-                if (page > 0) $(window).scrollTop(200);
-            });
-        }
-        else alert('Для пошуку потрібно мінімум 2 символи');
-    }
-    // Вибираємо елемент, який ми хочемо відстежувати
-    const targetElement = document.getElementById("magaz");
+			// Перевіряємо, чи знайдено числову відповідність
+			if (match && match[1]) {
+				// Повертаємо число в дужках як ціле число
+				return parseInt(match[1], 10);
+			} else {
+				// Якщо число в дужках не знайдено, повертаємо null або можна вказати інше значення за замовчуванням
+				return null;
+			}
+		};
+		let generateTable = (data) => {
+			console.log(data);
+			data.forEach((rowData) => {
+				let images = "";
+				rowData.imagesSrc.forEach((imgSrc) => {
+					images += `
+					<a href="${getLink(imgSrc)}"><img src="${imgSrc}"></a>`;
+				});
+				let table = `<div class='table'>
+						<div class='order-num fx'>
+							<p class='table-desc'>Номер Замовлення</p>
+							<p class='table-desc'>${rowData.orderNumber}</p>
+						</div>
+						<div class='order-manager fx'>
+							<p class='table-desc'>Менеджер</p>
+							<p class='table-desc'>${rowData.orderManager}</p>
+						</div>
+						<div class='good-desc fx'>
+							<p class='table-desc'>Товар</p>
+							<p class='table-desc'>${rowData.positionName}</p>
+						</div>
+						<div class='goods-adres fx'>
+							<p class='table-desc'>Адрес товару</p>
+							<p class='table-desc'>${rowData.positionPlace}</p>
+						</div>
+						<div class='goods-quality fx'>
+							<p class='table-desc'>Кількість</p>
+							<p class='table-desc'>${rowData.positionQuality}</p>
+						</div>
+						<div class='goods-reserve fx'>
+							<p class='table-desc'>Резерв</p>
+							<p class='table-desc'>${rowData.reserveQuality}</p>
+						</div>
+						<div class='order-anwser fx'>
+							<p class='table-desc'>Відповідь</p>
+							<div class='inp-wraper'><input
+                       type="text" class="elaboration-inp" id="elaborationInput348303"><button
+                        onclick="addElaborationAnswer(${getOrderId(
+													rowData.orderNumber
+												)},0)" class="elaboration-btn">збер.</button></div>
+						</div>
+						<div class='goods-photo fx'>
+							<p class='table-desc'>Фото товару</p>
+							<div class='goods-photo-wraper table-desc'>${images}</div>
+						</div>
+					</div>`;
+				newTableWraper.innerHTML = table;
+				let elaborationInp = newTableWraper.querySelector(".elaboration-inp");
+				let elaborationBtn = newTableWraper.querySelector(".elaboration-btn");
+			});
+		};
+		document.body.appendChild(newTableWraper);
+	};
+	// Вибираємо елемент, який ми хочемо відстежувати
+	const targetElement = document.getElementById("magaz");
 
-    // Створюємо спостерігача за змінами
-    const observer = new MutationObserver(function (mutationsList, observer) {
-        for (const mutation of mutationsList) {
-            if (mutation.type === "childList" || mutation.type === "attributes") {
-                // Обробляємо зміни
-                let children = Array.from(mutation.target.querySelectorAll("div"));
-                children.forEach((el) => {
-                    if (el.textContent.includes == "Уточнення") {
-                        el.id = "elaborationLink";
-                        elaborationLink.addEventListener("click", loadElaboration);
-                    }
-                })
+	// Створюємо спостерігача за змінами
+	const observer = new MutationObserver(function (mutationsList, observer) {
+		for (const mutation of mutationsList) {
+			if (mutation.type === "childList" || mutation.type === "attributes") {
+				// Обробляємо зміни
+				let children = Array.from(mutation.target.querySelectorAll("div"));
+				children.forEach((el) => {
+					if (el.textContent.includes == "Уточнення") {
+						el.id = "elaborationLink";
+						elaborationLink.addEventListener("click", loadElaboration);
+					}
+				});
+			}
+		}
+	});
 
-            }
-        }
-    });
+	// Починаємо відстежування змін
+	observer.observe(targetElement, {
+		attributes: true, // Відстежувати зміни атрибутів
+		childList: true, // Відстежувати зміни в дочірніх елементах
+		characterData: true, // Відстежувати зміни текстового вмісту
+		subtree: true, // Відстежувати зміни в піддереві
+	});
 
-    // Починаємо відстежування змін
-    observer.observe(targetElement, {
-        attributes: true,           // Відстежувати зміни атрибутів
-        childList: true,            // Відстежувати зміни в дочірніх елементах
-        characterData: true,        // Відстежувати зміни текстового вмісту
-        subtree: true               // Відстежувати зміни в піддереві
-    });
-    fetch("https://baza.m-p.in.ua/ajax/orders.php", {
-        method: "post"
-    }).then((res) => {
-        return res.text(
-
-        )
-    }).then((res) => {
-        let a = parseGetedHTML(res)
-        console.log(a)
-    })
-
+	loadElaboration();
 }
-
