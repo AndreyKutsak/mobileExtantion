3cfcb07d6a09afb693e7045a8ff70837db76f9d1
AndriyKutsak <andriykutsak.ak@gmail.com>
2023-10-12 23:02:26 +0300

    test background working

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
-const interval = 60000;
-let check;
-let browser = function fetchDataAndNotify() {
+let activeTabUrl;
+function fetchDataAndNotify() {
 	fetch("https://baza.m-p.in.ua/ajax/magaz.php")
 		.then((response) => response.text())
 		.then((data) => {
@@ -25,36 +24,31 @@ let browser = function fetchDataAndNotify() {
 				message: "",
 			});
 		});
-};
-
-// Викликаємо функцію fetchDataAndNotify по інтервалу
-
-// Зауваження: цей код призначений для Chrome Extension API. Якщо ви використовуєте інший браузер або WebExtensions API, код може вимагати певних змін.
+}
 function scheduleFunction() {
-	console.log("Check time is sarting");
 	const currentTime = new Date();
 	const startHour = 9; // 9:00 AM
 	const endHour = 18; // 6:00 PM
-
-	if (currentTime.getHours() >= startHour && currentTime.getHours() < endHour) {
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		var activeTab = tabs[0];
+		activeTabUrl = activeTab.url;
+		console.log("activeTabUrl", activeTabUrl);
+	});
+	if (
+		currentTime.getHours() >= startHour &&
+		currentTime.getHours() < endHour &&
+		!activeTabUrl.contains("baza.m-p.in.ua")
+	) {
 		fetchDataAndNotify();
-	} else {
-		chrome.notifications.create({
-			type: "basic",
-			iconUrl: "icon.png", // Шлях до зображення для сповіщення
-			title: "Зараз не час для роботи розширення!",
-			message: "",
-		});
 	}
 }
 
 chrome.alarms.create("myAlarm", {
-	periodInMinutes: 1, // Періодичність в хвилинах (один раз в хвилину)
+	periodInMinutes: 1,
 });
 
-// Обробник події, який викликає вашу функцію при спрацюванні будильника
 chrome.alarms.onAlarm.addListener(function (alarm) {
 	if (alarm.name === "myAlarm") {
-		scheduleFunction(); // Викликайте вашу функцію, коли будильник спрацьовує
+		scheduleFunction();
 	}
 });
